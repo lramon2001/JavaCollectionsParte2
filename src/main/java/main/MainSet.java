727b6a094ba74ab0618c412230ebe7a3a268af9e
Lucas Ramon <lucas.ramon@aluno.unb.br>
2021-06-30 14:53:16 -0300

    feito

---
+++ src/main/java/main/MainSet.java
@@ -0,0 +1,22 @@
+package main;
+
+import models.Pessoa;
+
+import java.util.HashSet;
+
+public class MainSet {
+    public static void main(String[] args){
+        HashSet<Pessoa> pessoas = new HashSet<Pessoa>();
+        System.out.println(pessoas.add(new Pessoa(1,"Lucas Ramon")));
+        System.out.println(pessoas);
+        System.out.println(pessoas.add(new Pessoa(1,"Lucas Ramon")));
+        System.out.println(pessoas);
+        System.out.println(pessoas.add(new Pessoa(2,"Pedro")));
+        System.out.println(pessoas.add(new Pessoa(3,"João")));
+        System.out.println(pessoas.add(new Pessoa(4,"Paulo")));
+        System.out.println(pessoas);
+    }
+
+
+
+}
--- src/main/java/models/Pessoa.java
+++ src/main/java/models/Pessoa.java
@@ -1,4 +1,67 @@
 package models;
+import java.util.Objects;
+import java.lang.*;
+public class Pessoa implements Comparable<Pessoa>  {
+    private int id;
+    private String nome;
 
-public class Pessoa {
+    public Pessoa() {
+    }
+
+    public Pessoa(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pessoa)) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return getId() == pessoa.getId() && Objects.equals(getNome(), pessoa.getNome());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getNome());
+    }
+
+    @Override
+    public int compareTo(Pessoa o) {
+        //0 : Os objetos são considerados iguais;
+        //
+        if(this.getId()==o.getId()){
+            return 0;
+        }
+
+        if(this.getId()<o.getId()){
+            return -1;
+        }
+
+        return 1;
+        }
 }
