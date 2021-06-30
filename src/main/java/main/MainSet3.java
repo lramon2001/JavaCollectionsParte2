93efa6e4998281444baff5810173e22f57b670f7
Lucas Ramon <lucas.ramon@aluno.unb.br>
2021-06-30 17:00:36 -0300

    iniciando estudos de hashtables

--- src/main/java/main/MainSet3.java
+++ src/main/java/main/MainSet3.java
@@ -1,8 +1,12 @@
 package main;
 
-public class MainSet2 {
-    public static void main(String[] args){
-        Set<Pessoa> pessoas = new LinkedSet<Pessoa>();
+import models.Pessoa;
+
+import java.util.HashSet;
+
+public class MainSet implements {
+
+    Set<Pessoa> pessoas = new TreeSet<Pessoa>();
         System.out.println(pessoas.add(new Pessoa(1,"Lucas Ramon")));
         System.out.println(pessoas);
         System.out.println(pessoas.add(new Pessoa(1,"Lucas Ramon")));
@@ -11,5 +15,6 @@ public class MainSet2 {
         System.out.println(pessoas.add(new Pessoa(3,"João")));
         System.out.println(pessoas.add(new Pessoa(4,"Paulo")));
         System.out.println(pessoas);
-    }
+
+
 }
