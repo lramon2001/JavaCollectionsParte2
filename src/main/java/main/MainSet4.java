4734aad7860e3c6cb4940d3743c04013739629ef
Lucas Ramon <lucas.ramon@aluno.unb.br>
2021-07-01 14:08:11 -0300

    Aprendendo novos métodos da hashTable

--- src/main/java/main/MainSet4.java
+++ src/main/java/main/MainSet4.java
@@ -20,7 +20,33 @@ public class MainSet  {
             String chave = iterator.next();
             System.out.println(tabelaPessoas.get(chave).toString);
         }
+        // Enumeration Non-Fail-Fast
+        Enumeration<String> chaves = tabelaPessoas.keys();
+        while(chaves.hasMoreElements()){
+            String chave = chaves.nextElement();
+            System.out.println(tabelaPessoas.get(chave).toString());
+        }
+        Pessoa p = null;
+       /* if(tabelaPessoas.containsKey("legal")){
+            p= tabelaPessoas.get("legal");
+        } else {
+            p = new Pessoa (-1,"Não existe");
+        }
+        System.out.println(p);
 
+        */
+        Pessoa p = tabelaPessoas.getOrDefault("legal",new Pessoa(-1,"Não existe"));
+        System.out.println(p);
+        /*if(!tabelaPessoas.containsKey("legal")){
+            tabelaPessoas.put("legal",new Pessoa(1,"Lucas Ramon"));
+        }
+
+         */
+        tabelaPessoas.putIfAbsent("legal", new Pessoa (1,"Lucas Ramon"));
+        System.out.println(tabelaPessoas);
+        tabelaPessoas.forEach((chave,valor)->{
+            System.out.println(String.format("[%s] %s",chave,valor.toString()));
+        });
     }
 
 
