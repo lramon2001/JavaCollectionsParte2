33534fd8f2edac320b1d5c75e83953724892c56f
Lucas Ramon <lucas.ramon@aluno.unb.br>
2021-07-01 17:20:59 -0300

    Estudo de mapas

--- src/main/java/main/MainSet5.java
+++ src/main/java/main/MainSet5.java
@@ -6,6 +6,16 @@ import java.util.HashSet;
 
 public class MainSet  {
 public static void main(String args[]){
-
+    Map<String,Pessoa> mapaPessoas = new HashMap<String,Pessoa>();
+    System.out.println(mapaPessoas.put("legal", new Pessoa(1,"Lucas")));
+    System.out.println(mapaPessoas);
+    System.out.println(mapaPessoas.put("legal", new Pessoa(2,"Lucas Ramon")));
+    System.out.println(mapaPessoas);
+    mapaPessoas.put(null, new Pessoa(-1,"Nulo"));
+    System.out.println(mapaPessoas);
+    mapaPessoas.put(null, new Pessoa(-2,"Nulo 2"));
+    System.out.println(mapaPessoas);
+    mapaPessoas.put("teste",null);
+    System.out.println(mapaPessoas);
 }
 }
